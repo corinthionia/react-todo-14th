1eb71917570997c59ed2ec57565064e9dec8bb40
Joohyun Kim <corinthionia@gmail.com>
2021-10-02 03:12:38 +0900

    :recycle: refactor: refactor code

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,20 +4,22 @@ const ItemList = ({ items, deleteTodo, toggleTodo }) => {
   const todoList = items.filter((todoObj) => todoObj.isDone === false);
   const doneList = items.filter((todoObj) => todoObj.isDone === true);
 
+  const list = [
+    { item: todoList, title: `🌴 TODO (${todoList.length})` },
+    { item: doneList, title: `💐 DONE (${doneList.length})` },
+  ];
+
   return (
     <>
-      <MakeList
-        items={todoList}
-        title={`🌴 TODO (${todoList.length})`}
-        deleteTodo={deleteTodo}
-        toggleTodo={toggleTodo}
-      />
-      <MakeList
-        items={doneList}
-        title={`💐 DONE (${doneList.length})`}
-        deleteTodo={deleteTodo}
-        toggleTodo={toggleTodo}
-      />
+      {list.map((list, index) => (
+        <MakeList
+          key={index}
+          items={list.item}
+          title={list.title}
+          deleteTodo={deleteTodo}
+          toggleTodo={toggleTodo}
+        />
+      ))}
     </>
   );
 };
